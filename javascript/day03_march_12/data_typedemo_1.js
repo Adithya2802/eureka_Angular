e170767e7cc566dfa01e6ab0e3b4c4ec9fefb274
Adithya <aaa@you.com>
2025-03-14 11:18:08 -0400

    Practised datatype, primitives, null and undefined in Day03 Practised Temporal Dead Zone, functions, hoisting, strict mode

---
+++ javascript/day03_march_12/data_typedemo_1.js
@@ -0,0 +1,8 @@
+let a = 10; 
+console.log(a, typeof a);
+
+let b = 4.5;
+console.log(b, typeof b);
+
+
+
---
+++ javascript/day03_march_12/primitive_demo_1.js
@@ -0,0 +1,26 @@
+console.log(10==10);
+console.log()
+
+console.log()
+
+
+//variables demo
+var a = 10;
+let b   = 20;
+const c = 30;
+
+console.log(a, b, c);
+
+console.log(`a: ${a} b: ${b} c: ${c}`);
+
+a= 15;
+b-45;
+// c=90;
+
+console.log(`a: ${a} b: ${b} c: ${c}`);
+
+const students = ['Adithya','Ajay'];
+console.log(students);
+students.push('harika');
+console.log(students);
+students = ['hhh', 'ff'];
